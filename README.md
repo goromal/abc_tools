b91d526ce664f2fced5653c1ffbc4566626ab46e
Andrew Torgesen <andrew.torgesen@gmail.com>
2020-08-15 23:37:18 -0400

    untested

---
+++ README.md
@@ -0,0 +1,9 @@
+# ABC Tools
+
+Command-line tools for converting between music files that utilize the ![http://abcnotation.com/](ABC music notation language) in a unix-based environment. To install and use, make sure you have a C compiler, then run:
+
+```bash
+git clone --recurse-submodules https://github.com/goromal/abc_tools.git
+cd abc_tools
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" 
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd "$DIR/abcm2ps"
+./configure && make
+cd ..
+mv abcm2ps/abcm2ps . && chmod +x abcm2ps
+cd abcmidi
+./configure && make
+cd ..
+mv abcmidi/abc2abc . && chmod +x abc2abc
+mv abcmidi/abc2midi . && chmod +x abc2midi
+mv abcmidi/abcmatch . && chmod +x abcmatch
+mv abcmidi/midi2abc . && chmod +x midi2abc
